dbefce80b5bede0e51a6f4ecf461ed3a5c4cd311
Brennan <brennanheley@gmail.com>
2022-05-09 23:48:56 -0500

    worked on comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,49 +1,28 @@
-const loginFormHandler = async (event) => {
+const addCommentSubmit = async function (event) {
   event.preventDefault();
 
-  // Collect values from the login form
-  const commentPost = document.querySelector("#comment-form").value.trim();
-  // const password = document.querySelector('#password-login').value.trim();
+  const comment = document.querySelector("#comment");
+  const post = document.querySelector("#post-id");
 
-  if (commentPost) {
-    // Send a POST request to the API endpoint
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+  const input = [];
 
-    if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
+  const response = await fetch("/api/comment/", {
+    method: "POST",
+    body: JSON.stringify({
+      contents: comment.value,
+      post_id: post.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
 
-const commentFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/homepage");
+    alert("Added to the list!");
+  } else {
+    alert("Failed to post");
   }
 };
 
 document
-  .querySelector(".comment-form")
-  .addEventListener("submit", commentFormHandler);
+  .querySelector("#comment-form")
+  .addEventListener("submit", addCommentSubmit);
